ca623d888a0944599d5b6c9673000eb808596e8e
SKOLZ <gabriel.zanzotti@gmail.com>
2024-01-16 11:33:12 +0100

    added border to host card.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import Image from 'next/image';
 import styles from './styles.module.css';
 import { EpisodesSection } from './EpisodesSection';
 import { AboutSection } from './components/AboutSection';
 import { HeroSection } from './components/HeroSection';
-=======
-import Image from "next/image";
-import styles from "./styles.module.css";
-import { HeroSection } from "./HeroSection";
-import { AboutSection } from "./AboutSection";
-import { EpisodesSection } from "./EpisodesSection";
->>>>>>> 9b3547af14a04fba8273295a010b56ecd6d6e7d1
 
 export default async function Home() {
   return (
