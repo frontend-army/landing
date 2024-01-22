7c83739891b8fdb3008e51d6cf2cb643cee5d2cc
FrankIglesias <ifrancisco.iglesias@gmail.com>
2024-01-22 19:39:59 +0100

    Added access token to prismic

--- src/app/episodes/page.tsx
+++ src/app/episodes/page.tsx
@@ -5,7 +5,7 @@ export default async  function Episodes() {
   const episodes = await getEpisodes();
   return (
     <div className="grid grid-cols-2 gap-6 max-w-[1024px] mx-auto">
-      {episodes.map((episode) => (
+      {episodes?.map((episode) => (
         <EpisodeCard key={episode.id} episode={episode} />
       ))}
     </div>
