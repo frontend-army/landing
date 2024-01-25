950e1f15fe46a585d229d748652affb3f28f2db2
FrankIglesias <ifrancisco.iglesias@gmail.com>
2024-01-25 16:22:56 +0100

    Corrected glitch

--- src/components/EpisodeCard/index.tsx
+++ src/components/EpisodeCard/index.tsx
@@ -4,20 +4,23 @@ import { Episode } from "@/types/episode";
 import styles from './styles.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { sourceIconMap } from "./utils";
-import { MouseEvent, MouseEventHandler } from "react";
+import { MouseEvent } from "react";
 
 interface Props {
   episode: Episode
 };
 
 export const EpisodeCard: React.FC<Props> = ({episode}) => {
-  const hadleOnMouseLeave = (e: MouseEvent<HTMLAnchorElement>) => {
+  const handleMouseLeave = (e: MouseEvent<HTMLAnchorElement>) => {
     e.currentTarget.classList.add(styles.buttonGlitch);
   }
 
-  const hadleOnMouseEnter = (e: MouseEvent<HTMLAnchorElement>) => {
-    e.currentTarget.classList.remove(styles.buttonGlitch);
+  const handleMouseEnter = () => {
+    document.querySelectorAll(`.${styles.buttonGlitch}`).forEach((element) => {
+      element.classList.remove(styles.buttonGlitch);
+    });
   }
+
 
   return (
     <div className={styles.episode}>
@@ -25,11 +28,11 @@ export const EpisodeCard: React.FC<Props> = ({episode}) => {
       <h3 className={styles.episodeTitle}>{episode.title}</h3>
       <p className={styles.episodeDescription}>{episode.description}</p>
       <div className={styles.episodeLinks}>
-        <a className={`${styles.episodeLink} ${styles.youtube}`} onMouseEnter={hadleOnMouseEnter} onMouseLeave={hadleOnMouseLeave} href={episode.youtube_url}>
+        <a className={`${styles.episodeLink} ${styles.youtube}`} onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter} href={episode.youtube_url}>
           <FontAwesomeIcon icon={sourceIconMap.youtube} />
           <span className={styles.episodeSourceName}>Youtube</span>
         </a>
-        <a className={`${styles.episodeLink} ${styles.spotify}`} onMouseEnter={hadleOnMouseEnter} onMouseLeave={hadleOnMouseLeave} href={episode.spotify_url}>
+        <a className={`${styles.episodeLink} ${styles.spotify}`} onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter} href={episode.spotify_url}>
           <FontAwesomeIcon icon={sourceIconMap.spotify} />
           <span className={styles.episodeSourceName}>Spotify</span>
         </a>
