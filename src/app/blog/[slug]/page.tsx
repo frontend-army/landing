2e63631b4ad8219f81a9cf77c2efa925f54ea827
FrankIglesias <ifrancisco.iglesias@gmail.com>
2024-02-01 18:19:36 +0100

    Added posts

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { fetchPost, fetchPosts } from '@/app/api/blogpost';
+
+export async function generateStaticParams() {
+  const posts = await fetchPosts();
+  return posts.map((post) => ({
+    slug: post.id,
+  }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string; };
+}) {
+  const { metadata } = await fetchPost(params.slug);
+  if (!metadata) {
+    return {};
+  }
+  return {
+    title: metadata.title,
+    description: metadata.description,
+  };
+}
+
+export default async function Post({
+  params,
+}: {
+  params: { slug: string; };
+}) {
+  const { source } = await fetchPost(params.slug);
+  return (
+      <article>
+          <MDXRemote
+            source={source}
+          />
+      </article>
+  );
+}
