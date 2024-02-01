fec24549845fb0b1022d065b68d79f4a776147fa
SKOLZ <gabriel.zanzotti@gmail.com>
2024-02-01 17:31:07 +0100

    improved the about section and footer.

--- src/app/home/components/AboutSection/components/HostCard/index.tsx
+++ src/app/home/components/AboutSection/components/HostCard/index.tsx
@@ -11,18 +11,20 @@ interface Props {
 export const HostCard: React.FC<Props> = ({host}) => {
   return (
     <div className={styles.host}>
-      <Image className={styles.hostAvatar} src={host.avatar} alt={`${host.name}'s profile picture`} width={220} height={200} />
-      <div className={styles.hostSocials}>
-        {
-          host.socials.map(social =>(
-            <a key={social.url} className={styles.hostSocial} href={social.url} target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={socialsIconMap[social.name.toLocaleLowerCase()]} />
-            </a>
-          ))
-        }
+      <Image className={styles.hostAvatar} src={host.avatar} alt={`${host.name}'s profile picture`} width={240} height={300} />
+      <div className={styles.hostInfo}>
+        <div className={styles.hostSocials}>
+          {
+            host.socials.map(social =>(
+              <a key={social.url} className={styles.hostSocial} href={social.url} target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={socialsIconMap[social.name.toLocaleLowerCase()]} />
+              </a>
+            ))
+          }
+        </div>
+        <p className={styles.hostName}>{host.name}</p>
+        <p className={styles.hostDescription}>{host.description}</p>
       </div>
-      <p className={styles.hostName}>{host.name}</p>
-      <p className={styles.hostDescription}>{host.description}</p>
     </div>
   );
 } 
