e1370484cd82ec8fcef25d83db4c65600bdc6cbf
FrankIglesias <ifrancisco.iglesias@gmail.com>
2024-02-06 10:11:27 +0100

    Added logo to meta tags

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,17 +10,23 @@ export const metadata = {
   description: 'Sumate a nuestro podcast y comunidad para explorar las tendencias en desarrollo web y compartir tu experiencia en el mundo Frontend.',
   image: FeaLogo,
   url: 'https://www.frontend-army.com',
+  openGraph: {
+    title: 'Frontend army',
+    description: 'Sumate a nuestro podcast y comunidad para explorar las tendencias en desarrollo web y compartir tu experiencia en el mundo Frontend.',
+    images: ["/fea_logo.png"],
+    url: 'https://www.frontend-army.com'
+  },
   twitter: {
     card : 'summary_large_image',
     title: 'Frontend army',
     description: 'Sumate a nuestro podcast y comunidad para explorar las tendencias en desarrollo web y compartir tu experiencia en el mundo Frontend.',
-    image: FeaLogo,
+    images: ["/fea_logo.png"],
     url: 'https://www.frontend-army.com'
   },
   linkedin: {
     title: 'Frontend army',
     description: 'Sumate a nuestro podcast y comunidad para explorar las tendencias en desarrollo web y compartir tu experiencia en el mundo Frontend.',
-    image: FeaLogo,
+    images: ["/fea_logo.png"],
     url: 'https://www.frontend-army.com'
   }
 }
