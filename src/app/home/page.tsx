0152a99bf5721b0df339f754257d8a03bcf590f6
FrankIglesias <ifrancisco.iglesias@gmail.com>
2024-02-06 10:00:42 +0100

    Added open graph to home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,27 @@ import { HeroSection } from './components/HeroSection';
 import { EpisodesSection } from './components/EpisodesSection';
 import { BlogSection } from './components/BlogSection';
 import { DotBackground } from './components/DotBackground';
+import FeaLogo from '@/assets/fea_logo.png';
+
+export const metadata = {
+  title: 'Frontend army',
+  description: 'Sumate a nuestro podcast y comunidad para explorar las tendencias en desarrollo web y compartir tu experiencia en el mundo Frontend.',
+  image: FeaLogo,
+  url: 'https://www.frontend-army.com',
+  twitter: {
+    card : 'summary_large_image',
+    title: 'Frontend army',
+    description: 'Sumate a nuestro podcast y comunidad para explorar las tendencias en desarrollo web y compartir tu experiencia en el mundo Frontend.',
+    image: FeaLogo,
+    url: 'https://www.frontend-army.com'
+  },
+  linkedin: {
+    title: 'Frontend army',
+    description: 'Sumate a nuestro podcast y comunidad para explorar las tendencias en desarrollo web y compartir tu experiencia en el mundo Frontend.',
+    image: FeaLogo,
+    url: 'https://www.frontend-army.com'
+  }
+}
 
 export default async function Home() {
   return (
