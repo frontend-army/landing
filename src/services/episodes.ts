5eb87f7c6bb46f51a9a835a0d1afea733704144e
FrankIglesias <ifrancisco.iglesias@gmail.com>
2024-02-07 21:18:58 +0100

    Generated episode pages

--- src/services/episodes.ts
+++ src/services/episodes.ts
@@ -4,3 +4,8 @@ export async function getEpisodes(limit?: number) {
   const { data: episodes } = await supabase.from("episodes").select("*").order("id", { ascending: false }).limit(limit as number);
   return episodes;
 }
+
+export async function getEpisode(id: number) {
+  const { data: episodes } = await supabase.from("episodes").select("*").order("id", { ascending: false }).eq("id", id);
+  return episodes ? episodes[0]: null;
+}
