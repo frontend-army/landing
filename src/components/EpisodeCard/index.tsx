a9b7d57c68aa96be70ed93b91f1729071ae5aac5
francisco-iglesias-deel <francisco.iglesias@deel.com>
2024-02-08 17:07:36 +0100

    Target blank on youtube and spotify

--- src/components/EpisodeCard/index.tsx
+++ src/components/EpisodeCard/index.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
 import { Episode } from "@/types/episode";
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { sourceIconMap } from "./utils";
 import { MouseEvent, useCallback, useEffect, useRef } from "react";
 
 interface Props {
-  episode: Episode
-};
+  episode: Episode;
+}
 
-export const EpisodeCard: React.FC<Props> = ({episode}) => {
+export const EpisodeCard: React.FC<Props> = ({ episode }) => {
   const youtubeRef = useRef<HTMLAnchorElement>(null);
   const spotifyRef = useRef<HTMLAnchorElement>(null);
 
   const handleMouseLeave = (e: MouseEvent<HTMLAnchorElement>) => {
     e.currentTarget.classList.add(styles.buttonGlitch);
-  }
+  };
 
   const handleMouseEnter = useCallback(() => {
     if (!youtubeRef.current || !spotifyRef.current) {
@@ -26,22 +26,37 @@ export const EpisodeCard: React.FC<Props> = ({episode}) => {
     spotifyRef.current.classList.remove(styles.buttonGlitch);
   }, []);
 
-
   return (
     <div className={styles.episode}>
       <h4 className={styles.episodeNumber}>Capitulo {episode.id}:</h4>
       <h3 className={styles.episodeTitle}>{episode.title}</h3>
       <p className={styles.episodeDescription}>{episode.description}</p>
       <div className={styles.episodeLinks}>
-        <a className={`${styles.episodeLink} ${styles.youtube}`} ref={youtubeRef} onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter} href={episode.youtube_url}>
+        <a
+          className={`${styles.episodeLink} ${styles.youtube}`}
+          ref={youtubeRef}
+          onMouseLeave={handleMouseLeave}
+          onMouseEnter={handleMouseEnter}
+          href={episode.youtube_url}
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon icon={sourceIconMap.youtube} />
           <span className={styles.episodeSourceName}>Youtube</span>
         </a>
-        <a className={`${styles.episodeLink} ${styles.spotify}`} ref={spotifyRef} onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter} href={episode.spotify_url}>
+        <a
+          className={`${styles.episodeLink} ${styles.spotify}`}
+          ref={spotifyRef}
+          onMouseLeave={handleMouseLeave}
+          onMouseEnter={handleMouseEnter}
+          href={episode.spotify_url}
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon icon={sourceIconMap.spotify} />
           <span className={styles.episodeSourceName}>Spotify</span>
         </a>
       </div>
     </div>
   );
-} 
+};
