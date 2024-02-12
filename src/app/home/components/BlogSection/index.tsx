01bfa5177824dade7ddadf2960599fcf8b45a079
SKOLZ <gabriel.zanzotti@gmail.com>
2024-02-12 17:09:40 +0100

    added animations to titles and improved see more episodes button animation.

--- src/app/home/components/BlogSection/index.tsx
+++ src/app/home/components/BlogSection/index.tsx
@@ -2,6 +2,7 @@ import { fetchPosts } from "@/app/api/blogpost";
 import { BlogpostCard } from "./components/BlogpostCard";
 import { SeeMoreEpisodesButton } from "../EpisodesSection/components/SeeMoreEpisodesButton";
 import styles from './styles.module.css';
+import { SectionTitle } from "@/components/SectionTitle";
 
 export const revalidate = 3600;
 
@@ -9,7 +10,7 @@ export const BlogSection: React.FC = async () => {
   const posts = await fetchPosts();
   return (
     <section className={`${styles.blogSection} flex flex-col items-center`}>
-      <h2 className="title-1 mb-16">Nuestro Blog</h2>
+      <SectionTitle className="mb-16">Nuestro Blog</SectionTitle>
       <div className={`${styles.posts} mb-12`}>
         {posts?.map((postSummary, index) => (
           <BlogpostCard key={postSummary.title} postSummary={postSummary} isHorizontal={index > 0} />
--- src/app/home/components/EpisodesSection/components/SeeMoreEpisodesButton/index.tsx
+++ src/app/home/components/EpisodesSection/components/SeeMoreEpisodesButton/index.tsx
@@ -4,19 +4,13 @@ import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 import { useRef } from "react";
 import styles from './styles.module.css';
 import ChevronRight from "@/assets/chevron-right.svg";
+import { useAnimateOnScroll } from "@/hooks/useRevealOnScroll";
 
 export const SeeMoreEpisodesButton: React.FC = () => {
-  const buttonRef = useRef(null);
-  const buttonObserver = useIntersectionObserver(buttonRef, {
-    threshold: 1,
-    // freezeOnceVisible: true,
-    rootMargin: '0px 0px -50px 0px',
-  });
-
-  const isButtonVisible = !!buttonObserver?.isIntersecting;
+  const {ref: buttonRef, isVisible: isButtonVisible} = useAnimateOnScroll('50px');
 
   return (
-    <button ref={buttonRef} className={`self-end ${isButtonVisible ? styles.postsSeeMore : 'opacity-0'}`} type="button">
+    <button ref={buttonRef} className={`${styles.postsSeeMore} ${isButtonVisible ? styles.isActive : 'opacity-0'} self-end`} type="button">
       <span className={styles.postsSeeMoreContent}>
           Ver mas capítulos
           <ChevronRight className={styles.postsSeeMoreArrow} width={24} height={24} alt="" />
