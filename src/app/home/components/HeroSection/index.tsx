6292bdd89e4b9e1483d16934d1a525139e9f9a51
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2024-04-23 10:39:47 +0200

    Update index.tsx

--- src/app/home/components/HeroSection/index.tsx
+++ src/app/home/components/HeroSection/index.tsx
@@ -8,7 +8,7 @@ export const HeroSection: React.FC = () => {
     <section className={styles.hero}>
       <div className={styles.heroBg} />
       <h1 className={`${styles.heroTitle} glitch`} data-text="Frontend Army">
-        Frontend Army
+        Frontend Armies
       </h1>
       <TypewriterParagraph className={styles.heroDescription}>
         Sumate a nuestro podcast y comunidad para explorar las tendencias en desarrollo web y compartir tu experiencia en el mundo Frontend.
