262d9ab39fe33a65dd9d81480150cd91f402d40f
Pablo Ferro <pablo.pyp@gmail.com>
2025-06-09 20:28:52 -0300

    randomize on client

--- src/components/NotFoundAnimation/index.tsx
+++ src/components/NotFoundAnimation/index.tsx
@@ -1,26 +1,34 @@
-'use client'
-import Image from 'next/image'
-import capiWow from '@/assets/capi-wow.png'
-import tinchoWow from '@/assets/tincho-wow.png'
-import pablitoWow from '@/assets/pablito-wow.png'
-import { motion } from 'motion/react'
+"use client";
+import Image from "next/image";
+import capiWow from "@/assets/capi-wow.png";
+import tinchoWow from "@/assets/tincho-wow.png";
+import pablitoWow from "@/assets/pablito-wow.png";
+import { motion } from "motion/react";
+import { useEffect, useState } from "react";
 
 const images = [
-    { src: capiWow, alt: 'Capi', width: 320 },
-    { src: tinchoWow, alt: 'Tincho', width: 200 },
-    { src: pablitoWow, alt: 'Pablito', width: 200 }
-]
+  { src: capiWow, alt: "Capi", width: 320 },
+  { src: tinchoWow, alt: "Tincho", width: 200 },
+  { src: pablitoWow, alt: "Pablito", width: 200 },
+];
 
 export default function NotFoundAnimation() {
-    const image = images[Math.floor(Math.random() * images.length)];
-    return (
-        <motion.div
-            className='absolute'
-            initial={{ opacity: 0, bottom: -300 }}
-            animate={{ opacity: 1, bottom: 0 }}
-            transition={{ duration: 0.5 }}
-        >
-            <Image src={image.src} alt={image.alt} width={image.width} />
-        </motion.div>
-    )
+  const [image, setImage] = useState<(typeof images)[0] | null>(null);
+
+  useEffect(() => {
+    setImage(images[Math.floor(Math.random() * images.length)]);
+  }, []);
+
+  if (!image) return null;
+
+  return (
+    <motion.div
+      className="absolute"
+      initial={{ opacity: 0, bottom: -300 }}
+      animate={{ opacity: 1, bottom: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Image src={image.src} alt={image.alt} width={image.width} />
+    </motion.div>
+  );
 }
