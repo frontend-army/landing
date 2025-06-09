e5876303e9b1999d268b431b96e1aac0ec350615
Pablo Ferro <pablo.pyp@gmail.com>
2025-06-09 19:36:35 -0300

    Add 404 page with random faces

---
+++ src/components/NotFoundAnimation/index.tsx
@@ -0,0 +1,26 @@
+'use client'
+import Image from 'next/image'
+import capiWow from '@/assets/capi-wow.png'
+import tinchoWow from '@/assets/tincho-wow.png'
+import pablitoWow from '@/assets/pablito-wow.png'
+import { motion } from 'motion/react'
+
+const images = [
+    { src: capiWow, alt: 'Capi', width: 320 },
+    { src: tinchoWow, alt: 'Tincho', width: 200 },
+    { src: pablitoWow, alt: 'Pablito', width: 200 }
+]
+
+export default function NotFoundAnimation() {
+    const image = images[Math.floor(Math.random() * images.length)];
+    return (
+        <motion.div
+            className='absolute'
+            initial={{ opacity: 0, bottom: -300 }}
+            animate={{ opacity: 1, bottom: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            <Image src={image.src} alt={image.alt} width={image.width} />
+        </motion.div>
+    )
+}
